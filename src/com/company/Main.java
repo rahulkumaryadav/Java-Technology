f7704ac3d9e3ddb2fb82d3978b5577818b0d352a
Rahul kumar <rahulkumar96369@gmail.com>
2018-01-15 21:02:05 +0530

    java syllabus is commited

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,5 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");    }
+        System.out.println("Welcome to java technology");    }
 }
