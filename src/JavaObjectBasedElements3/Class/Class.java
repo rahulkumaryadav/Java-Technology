c43365d2fa2609f094d93a82fb182a1ed0bc8b2b
Rahul kumar <rahulkumar96369@gmail.com>
2018-01-16 23:49:53 +0530

    syllabus commited

--- src/JavaObjectBasedElements3/Class/Class.java
+++ src/JavaObjectBasedElements3/Class/Class.java
@@ -1,4 +1,11 @@
 package JavaObjectBasedElements3.Class;
 
 public class Class {
+    public static void main(String[] args) {
+        System.out.println("For Example1 of class please visit the following link");
+    }
+    /*
+    https://www.guru99.com/java-oops-class-objects.html
+     */
+
 }
